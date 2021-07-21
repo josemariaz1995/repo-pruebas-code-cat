955252c5376913cc1acd46d28fffefc3e2847a3f
Jose Maria Zamora <jzamora@biada.net>
2021-07-21 22:04:00 +0200

    datos del usuario mostrados en pantalla y barra de experienca segun la experienca del usaurio y con un maximo fijo

--- src/components/CodeCat.js
+++ src/components/CodeCat.js
@@ -2,50 +2,50 @@ import { useContext } from "react";
 import { useEffect } from "react";
 import { useCallback } from "react";
 import { useState } from "react";
-import { Col, Row } from "react-bootstrap";
+import { Col, ProgressBar, Row } from "react-bootstrap";
 import { AuthContext } from "../context/AuthContext";
 
 export const CodeCat = () => {
   const [datosUsuario, setDatosUsuario] = useState([]);
-
-  const { token, urlApi } = useContext(AuthContext);
+  const { token, urlApi, setDatosFormaciones } = useContext(AuthContext);
 
   const obtenerDatosUsuario = useCallback(async () => {
     try {
-      const response = await fetch(urlApi + "usuarios/datos", {
+      const response = await fetch(urlApi + "codecat/cargar-informacion", {
         headers: {
           Authorization: "Bearer " + token,
         },
       });
       const usuario = await response.json();
       setDatosUsuario(usuario);
+      setDatosFormaciones([...usuario.listadoFormaciones]);
     } catch (error) {
       console.log(error);
     }
-  }, [token, urlApi]);
-
+  }, [setDatosFormaciones, token, urlApi]);
+  useEffect(() => obtenerDatosUsuario(), [obtenerDatosUsuario]);
   return (
     <Row>
-      {/* {datosTrabajo.map((dato, index) => (
-        <>
-          <Col key={index} xs="12">
-            Nombre Trabajo: {dato.nombre}
-          </Col>
-          {dato.tareas.map((tarea) => (
-            <Col>Tarea: {tarea.nombre}</Col>
-          ))}
-        </>
-      ))}
-      {datosFormaciones.map((dato, index) => (
-        <>
-          <Col key={index} xs="12">
-            Nombre Formacion: {dato.nombre}
-          </Col>
-          {dato.tareas.map((tarea) => (
-            <Col>Tarea Formacion: {tarea.nombre}</Col>
-          ))}
-        </>
-      ))} */}
+      <Col>nivel: {datosUsuario?.nivelUsuario?.nivel}</Col>
+      <ProgressBar
+        className="progress p-0"
+        striped
+        variant="info"
+        label={
+          Math.floor((datosUsuario?.usuario?.experiencia * 100) / 700) + "%"
+        }
+        now={datosUsuario?.usuario?.experiencia}
+        min={0}
+        max={700}
+      />
+      <Col>Titulo: {datosUsuario?.nivelUsuario?.titulo}</Col>
+
+      <Col>Username: {datosUsuario?.usuario?.username}</Col>
+      <Col>Exp: {datosUsuario?.usuario?.experiencia}</Col>
+      <Col>
+        galletitas:
+        {datosUsuario?.usuario?.galletas ? datosUsuario.usuario.galletas : 0}
+      </Col>
     </Row>
   );
 };
